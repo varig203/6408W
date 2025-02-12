606947ab358a3787f28331564706a04bcdc1c9fc
Radin <radin@rngfx.com>
2025-02-12 15:38:11 -0800

    gearbox header

---
+++ gearbox.hpp
@@ -0,0 +1,9 @@
+#ifndef GEARBOX_HPP
+#define GEARBOX_HPP
+
+// Declaration for gearbox control functions
+
+// Runs the gearbox control loop that handles the intake, PID arm load, and outtake functions.
+void Controll_Gears();
+
+#endif // GEARBOX_HPP 
