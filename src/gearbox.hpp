abbec15319b4c064b10600177bc03a3dd6ce95fa
Radin <radin@rngfx.com>
2025-02-12 15:45:11 -0800

    removed dupes

--- src/gearbox.hpp
+++
@@ -1,9 +0,0 @@
-#ifndef GEARBOX_HPP
-#define GEARBOX_HPP
-
-// Declaration for gearbox control functions
-
-// Runs the gearbox control loop that handles the intake, PID arm load, and outtake functions.
-void Controll_Gears();
-
-#endif // GEARBOX_HPP 
