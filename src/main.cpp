0d69b6d35c37426011ed87a741f47c9c1bb7bf61
Radin <radin@rngfx.com>
2025-02-11 16:10:33 -0800

    drivetrain?

--- src/main.cpp
+++ src/main.cpp
@@ -75,9 +75,8 @@ void autonomous() {}
  */
 void opcontrol() {
 	pros::Controller master(pros::E_CONTROLLER_MASTER);
-	pros::MotorGroup left_mg({1, -2, 3});    // Creates a motor group with forwards ports 1 & 3 and reversed port 2
-	pros::MotorGroup right_mg({-4, 5, -6});  // Creates a motor group with forwards port 5 and reversed ports 4 & 6
-
+	pros::MotorGroup left_mg({-5, 6, -7});    // Left motors: reversed port 5, forward port 6, reversed port 7
+	pros::MotorGroup right_mg({1, -3, 4});    // Right motors: forward port 1, reversed port 3, forward port 4
 
 	while (true) {
 		pros::lcd::print(0, "%d %d %d", (pros::lcd::read_buttons() & LCD_BTN_LEFT) >> 2,
