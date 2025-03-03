2d9cbe89e71a153ea24aacb829189d9f958d9c94
Daniel TB <varig203@gmail.com>
2025-03-02 21:38:46 -0800

    Shortened comments

--- src/main.cpp
+++ src/main.cpp
@@ -60,12 +60,7 @@ void pneumatics_fn() {
 	}
 }
 
-/**
- * Runs initialization code. This occurs as soon as the program is started.
- *
- * All other competition modes are blocked by initialize; it is recommended
- * to keep execution time for this mode under a few seconds.
- */
+// Runs initialization code when the program starts; all other competition modes are blocked, keep exec under few secs
 void initialize() {
 	// Set up drivetrain brake modes
 	LeftMotors.set_brake_mode(pros::E_MOTOR_BRAKE_BRAKE);
@@ -79,52 +74,20 @@ void initialize() {
 	autonSelector.focus();
 }
 
-/**
- * Runs while the robot is in the disabled state of Field Management System or
- * the VEX Competition Switch, following either autonomous or opcontrol. When
- * the robot is enabled, this task will exit.
- */
+
+// Runs while the robot is disabled, following autonomous or opcontrol, and exits when the robot is enabled.
 void disabled() {}
 
-/**
- * Runs after initialize(), and before autonomous when connected to the Field
- * Management System or the VEX Competition Switch. This is intended for
- * competition-specific initialization routines, such as an autonomous selector
- * on the LCD.
- *
- * This task will exit when the robot is enabled and autonomous or opcontrol
- * starts.
- */
+// Runs after initalize and before auton. only when connected to field control
 void competition_initialize() {}
 
-/**
- * Runs the user autonomous code. This function will be started in its own task
- * with the default priority and stack size whenever the robot is enabled via
- * the Field Management System or the VEX Competition Switch in the autonomous
- * mode. Alternatively, this function may be called in initialize or opcontrol
- * for non-competition testing purposes.
- *
- * If the robot is disabled or communications is lost, the autonomous task
- * will be stopped. Re-enabling the robot will restart the task, not re-start it
- * from where it left off.
- */
+// auton
 void autonomous() {
 	//Large_Balls_To_Lick(); // skills 
 	autonSelector.run_auton(); // REMOVE COMMENT WHEN READY AT COMP
 }
-/**
- * Runs the operator control code. This function will be started in its own task
- * with the default priority and stack size whenever the robot is enabled via
- * the Field Management System or the VEX Competition Switch in the operator
- * control mode.
- *
- * If no competition control is connected, this function will run immediately
- * following initialize().
- *
- * If the robot is disabled or communications is lost, the
- * operator control task will be stopped. Re-enabling the robot will restart the
- * task, not resume it from where it left off.
- */
+
+// Runs the operator control code in its own task when the robot is enabled, stops if disabled or comms lost.
 void opcontrol() {
 	// Calls all the threads to start
 	pros::Task chassis_task(chassis_fn);
